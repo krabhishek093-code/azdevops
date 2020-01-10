315f29fa4ad390906483b9bb492d55968891bdd7
zippyops <zippyops@gmail.com>
2020-01-10 08:28:54 +0000

    Updated AppserviceJava.tf

--- Terraform/AppserviceJava.tf
+++ Terraform/AppserviceJava.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "java" {
 
 resource "azurerm_app_service_plan" "javaserviceplan" {
   name                = "java-appserviceplan"
-  location            = "${azurerm_resource_group.java.location}"
+  location            = azurerm_resource_group.java.location
   resource_group_name = "${azurerm_resource_group.java.name}"
   kind                = "Linux"
   reserved            = true
