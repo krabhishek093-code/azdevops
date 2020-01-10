48ed52fc66a1be7af0f84c28c2d32591227bdd0f
zippyops <zippyops@gmail.com>
2020-01-10 04:58:55 +0000

    Updated AppserviceJava.tf

--- Terraform/AppserviceJava.tf
+++ Terraform/AppserviceJava.tf
@@ -3,33 +3,34 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=1.38.0"
 }
-
 resource "azurerm_resource_group" "java" {
   name     = "javasample"
   location = "WestUS"
 }
 
-resource "azurerm_app_service_plan" "java" {
+resource "azurerm_app_service_plan" "javaserviceplan" {
   name                = "java-appserviceplan"
   location            = "${azurerm_resource_group.java.location}"
   resource_group_name = "${azurerm_resource_group.java.name}"
-  kind                = "linux"
+  kind                = "Linux"
   reserved            = true
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
-resource "azurerm_app_service" "java" {
-  name                = "java-app-service"
+resource "azurerm_app_service" "samplejava" {
+  name                = "samplejava-app-service"
   location            = "${azurerm_resource_group.java.location}"
   resource_group_name = "${azurerm_resource_group.java.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.java.id}"
+  app_service_plan_id = "${azurerm_app_service_plan.javaserviceplan.id}"
 
   site_config {
-    java_version 
-    java_container_version
-    java_container
+    java_container = "JAVA"
+    java_container_version ="11"
+    use_32_bit_worker_process = true
+
   }
 }
+
