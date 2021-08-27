4a4fafe2e36f33f59bea38a37c4cb51c78c1ff29
zippyops <zippyops@gmail.com>
2021-08-27 12:15:04 +0000

    Updated AppserviceJava.tf

--- Terraform/AppserviceJava.tf
+++ Terraform/AppserviceJava.tf
@@ -1,13 +1,34 @@
 # Configure the Azure Provider
-provider "azurerm" {
+#provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=1.38.0"
+#  version = "=1.38.0"
+#}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstatevs12y"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "java" {
   name     = "javasample"
   location = "WestUS"
 }
 
+# Service Plan Creation 
 resource "azurerm_app_service_plan" "javaserviceplan" {
   name                = "java-appserviceplan"
   location            = "${azurerm_resource_group.java.location}"
@@ -20,6 +41,8 @@ resource "azurerm_app_service_plan" "javaserviceplan" {
   }
 }
 
+
+# Creation of App service
 resource "azurerm_app_service" "samplejava" {
   name                = "samplejava-app-service"
   location            = "${azurerm_resource_group.java.location}"
